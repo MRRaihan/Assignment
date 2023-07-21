57dbd60d81a17bd8710d39c0bf7995cf6d454819
MRRaihan <raihantfp@gmail.com>
2023-07-21 11:15:45 +0600

    Create admin dashboard template

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,10 @@
  */
 
 import './bootstrap';
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
+
 import { createApp } from 'vue';
 
 /**
