a2eecfc6e34104d1c2993e5382ab30006c546a9d
MRRaihan <raihantfp@gmail.com>
2023-07-21 12:27:04 +0600

    Dashboard Component added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,4 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+
 
 import './bootstrap';
 import 'admin-lte/plugins/jquery/jquery.min.js';
@@ -10,34 +6,21 @@ import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
 import 'admin-lte/dist/js/adminlte.min.js';
 
 import { createApp } from 'vue';
-
-/**
- * Next, we will create a fresh Vue application instance. You may then begin
- * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
- */
+import { createRouter, createWebHistory } from 'vue-router';
+import Routes from './routes.js';
 
 const app = createApp({});
+
+const router = createRouter({
+
+    routes: Routes,
+    history: createWebHistory(),
+});
+
+
 
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-// });
-
-/**
- * Finally, we will attach the application instance to a HTML element with
- * an "id" attribute of "app". This element is included with the "auth"
- * scaffolding. Otherwise, you will need to add an element yourself.
- */
-
+app.use(router);
 app.mount('#app');
