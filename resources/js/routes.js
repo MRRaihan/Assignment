319e79c3ee2bc8693ec289f4260015f7d967d3aa
MRRaihan <raihantfp@gmail.com>
2023-07-21 14:53:04 +0600

    Create components for department, student & result list

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,4 +1,9 @@
 import Dashboard from './components/Dashboard.vue';
+import ListDepartments from './pages/departments/ListDepartments.vue';
+import ListStudents from './pages/students/ListStudents.vue';
+import ListResults from './pages/results/ListResults.vue';
+
+
 
 
 export default [
@@ -6,5 +11,20 @@ export default [
         path: '/admin/dashboard',
         name: 'admin.dashboard',
         component: Dashboard,
+    },
+    {
+        path: '/admin/departments',
+        name: 'admin.departments',
+        component: ListDepartments,
+    },
+    {
+        path: '/admin/students',
+        name: 'admin.students',
+        component: ListStudents,
+    },
+    {
+        path: '/admin/results',
+        name: 'admin.results',
+        component: ListResults,
     }
 ]
